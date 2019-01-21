0d017249f60b414b7fcb73e9590bbda3aee23832
Anjaneyulu Batta <anjaneyulu.batta505@gmail.com>
2019-01-22 00:13:54 +0530

    module and package added

---
+++ python/modules_and_packages/main.py
@@ -0,0 +1,13 @@
+from simple_math import add, substract
+
+print(add(100, 50))
+# output: 150
+print(substract(100, 50))
+# output: 50
+
+
+# package code
+from my_package import utils
+
+utils.say_hello("LearnBATTA")
+# output: LearnBATTA
