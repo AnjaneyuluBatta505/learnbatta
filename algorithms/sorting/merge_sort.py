aa816181cf285649e37eebe5a147616211c67ce6
Anjaneyulu Batta <anjaneyulu.batta505@gmail.com>
2019-07-09 18:58:42 +0530

    Update merge_sort.py

--- algorithms/sorting/merge_sort.py
+++ algorithms/sorting/merge_sort.py
@@ -8,36 +8,42 @@ Divide the unsorted list into n sublists, each containing 1 element (a list of 1
 Repeatedly merge sublists to produce new sorted sublists until there is only 1 sublist remaining. This will be the sorted list.
 '''
 
-def mergeSort(nlist):
-    print("Splitting ",nlist)
-    if len(nlist)>1:
-        mid = len(nlist)//2
-        lefthalf = nlist[:mid]
-        righthalf = nlist[mid:]
+def mergesort(arr):
+  arr_length = len(arr)
+  if(arr_length > 1):
+    middle = (arr_length)//2
+    left = arr[:middle]
+    right = arr[middle:]
+    mergesort(left)
+    mergesort(right)
+    left_length = len(left)
+    right_length = len(right)
+    i=j=k=0
+    while i < left_length and j < right_length:
+      if(left[i] < right[j]):
+        arr[k] = left[i]
+        i += 1
+      else:
+        arr[k] = right[j]
+        j += 1
+      k += 1
+    # update remaining sorted elements in left
+    while i < left_length:
+      arr[k] = left[i]
+      i += 1
+      k += 1
+    # update remaining sorted elements in right
+    while j < right_length:
+      arr[k] = right[j]
+      j += 1
+      k += 1
 
-        mergeSort(lefthalf)
-        mergeSort(righthalf)
-        i=j=k=0       
-        while i < len(lefthalf) and j < len(righthalf):
-            if lefthalf[i] < righthalf[j]:
-                nlist[k]=lefthalf[i]
-                i=i+1
-            else:
-                nlist[k]=righthalf[j]
-                j=j+1
-            k=k+1
 
-        while i < len(lefthalf):
-            nlist[k]=lefthalf[i]
-            i=i+1
-            k=k+1
-
-        while j < len(righthalf):
-            nlist[k]=righthalf[j]
-            j=j+1
-            k=k+1
-    print("Merging ",nlist)
-
-nlist = [14,46,43,27,57,41,45,21,70]
-mergeSort(nlist)
-print(nlist)
+if __name__ == '__main__':
+  arr = [4,2,1,5,6,8,7,3]
+  # arr = [4,2,1,5,3]
+  # arr = [4,2,1,3]
+  # arr = [2,1,3]
+  print("Initial arr: %s" % arr)
+  mergesort(arr)
+  print("ater merge sort arr: %s" % arr)
